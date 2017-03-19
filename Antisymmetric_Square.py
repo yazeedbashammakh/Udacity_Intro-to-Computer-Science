73a255617f78bf7b25e788cb53577d09ea61226a
yazeedbashammakh <yazeedbashammakh@gmail.com>
2017-03-20 00:11:13 +0530

    Create Antisymmetric_Square.py

---
+++ Antisymmetric_Square.py
@@ -0,0 +1,41 @@
+# By Dimitris_GR from forums
+# Modify Problem Set 31's (Optional) Symmetric Square to return True 
+# if the given square is antisymmetric and False otherwise. 
+# An nxn square is called antisymmetric if A[i][j]=-A[j][i] 
+# for each i=0,1,...,n-1 and for each j=0,1,...,n-1.
+
+def antisymmetric(s):
+    n = len(s)
+    if n == 0:
+        return True
+    m = len(s[0])
+    for i in range(n):
+        for j in range(m):
+            if j >= len(s[i]) or j >= n or i >= len(s[i]) or s[i][j] != -s[j][i]:
+                return False
+    return True        
+    
+    #Write your code here
+
+
+# Test Cases:
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, 3], 
+                     [-2, -3, 0]])   
+#>>> True
+
+print antisymmetric([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]])
+#>>> True
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, -2], 
+                     [2, 2,  3]])
+#>>> False
+
+print antisymmetric([[1, 2, 5],
+                     [0, 1, -9],
+                     [0, 0, 1]])
+#>>> False
